91fb92d6b658bd0f9eacf10259fac23616cbbe67
BrainFooLong <brainfoolong@users.noreply.github.com>
2016-12-06 10:48:24 +0100

    fix for install script

--- install.sh
+++ install.sh
@@ -9,43 +9,50 @@ echo "If you want the latest master branch, you have do that manually"
 echo "Are you want to continue? [y/n]"
 
 read p
+echo -e "\n\n\n\n\n"
 
 if [ "$p" != "y" ] && [ "$p" != "Y" ] ; then
     echo "Aborted"
     exit 1
 fi
 
+echo -e "\n\n\n\n\n"
 echo "OMXWEBGUI runs with php5 command line interface"
 echo "No separate webserver required and recommended"
 echo "Maybe you will some errors because of non existing packages, ignore it"
 echo "Are you want to install php5-cli now (sudo)? [y/n]"
 
 read p
+echo -e "\n\n\n\n\n"
 
 if [ "$p" = "y" ] || [ "$p" = "Y" ] ; then
     sudo apt-get install -qq -y php-cli
     sudo apt-get install -qq -y php5-cli
 fi
 
+echo -e "\n\n\n\n\n"
 echo "OMXWEBGUI runs as a php-cli server which requires a port to be defined"
 echo "Default port is 4321"
 echo "Leave it empty if you stick with the default or change it to a number you want"
 
 read p
+echo -e "\n\n\n\n\n"
 
 port=4321
 if [ "$p" != "" ] ; then
     port=$p
 fi
 
+echo -e "\n\n\n\n\n"
 echo "Downloading and unpacking OMXWEBGUI to disk"
 echo "Choose directory path, default: ~/omxwebgui"
 echo "Leave it empty if you stick with the default"
 echo "Given directory should be empty or non existing"
 
 read p
+echo -e "\n\n\n\n\n"
 
-path="C:\www\private\omxwebgui"
+path="~/omxwebgui"
 if [ "$p" != "" ] ; then
     path=$p
 fi
@@ -60,21 +67,29 @@ mkdir "$path/tmp"
 wget https://raw.githubusercontent.com/brainfoolong/omxwebgui-v2/master/updater.php
 php -f updater.php
 
+echo -e "\n\n\n\n\n"
 echo "Enable autostart for the OMXWEBGUI server"
 echo "This will add a new entry to crontab"
 echo "Are you want to enable autostart now? [y/n]"
+
+read p
+echo -e "\n\n\n\n\n"
 
 if [ "$p" = "y" ] || [ "$p" = "Y" ] ; then
     (crontab -l 2>/dev/null; echo "@reboot php -S 0.0.0.0:$port -t $path > /dev/null 2>&1 &") | crontab -
 fi
 
+echo -e "\n\n\n\n\n"
 echo "Start OMXWEBGUI server now? [y/n]"
+
+read p
+echo -e "\n\n\n\n\n"
 
 if [ "$p" = "y" ] || [ "$p" = "Y" ] ; then
     `php -S 0.0.0.0:$port -t $path > /dev/null 2>&1 &`
 fi
 
-
+echo -e "\n\n\n\n\n"
 echo "All files have been installed!"
 echo "Open http://iptoyourpi:$port in your browser"
 echo "Have fun with this application. You are awesome."
