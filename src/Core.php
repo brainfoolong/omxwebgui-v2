9362571d445da342934a4472488d42b3f52fce20
BrainFooLong <roland@bfldev.com>
2016-12-07 19:51:28 +0100

    version update

--- src/Core.php
+++ src/Core.php
@@ -78,7 +78,7 @@ namespace Nullix\Omxwebgui {
          *
          * @var string
          */
-        public static $version = "0.10.5";
+        public static $version = "0.10.6";
 
         /**
          * Initialize
